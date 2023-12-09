72709049819e2d3ca3a4e8118b4d11e66bb47095
govindraj945 <105144153+govindraj945@users.noreply.github.com>
2023-12-09 12:25:37 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as a newer version
+//alert(3+6) // we are using nodejs, not browser
+// code readablity is high
+// https://tc39.es/ecma262/    // ECMA script website
+console.log(3 + 3);
+console.log("govind");
+
+
+// data types
+let name = "hitesh"  // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+let state; // undefined
+
+// in Js there are two data types => 01 Primitive , 02 non-primitive.
+
+//01 Primitive 
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//02 Non-primitve
+// object
+//
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
