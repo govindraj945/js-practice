ce90510690cc63d1287d485ad7fb40b60451a693
govindraj945 <105144153+govindraj945@users.noreply.github.com>
2023-12-09 16:04:54 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,9 @@
 let score = "33"
 
-
 // console.log(typeof score);
 // console.log(typeof (score));
 
+//Number
 let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
@@ -12,6 +12,7 @@ let valueInNumber = Number(score);
 // "33abc" => NaN
 //  true => 1; false => 0
 
+// Boolean
 let isLoggedIn = "govind"
 
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
@@ -21,9 +22,69 @@ let isLoggedIn = "govind"
 // "" => false
 // "govind" => true
 
+//string
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3); //output 2  // if the numerator is less than denomenator the answer is the numerator value 
+
+
+// let num = 5
+// let str1 = "6"
+// let num2= Number(str1)
+// let ans = num + num2
+// console.log(ans);
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+//  in the js check 1 type the execute the next one.
+// console.log("1" + 2); // output 12
+// console.log(1 + "2"); // output 12
+// console.log("1" + 2 + 2); // output 122
+// console.log(1 + 2 + "2"); // output 32
+
+// console.log( (3 + 4) * 5 % 3);  // bad habit
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+// let s = 5
+// let g = s++
+// console.log(g);
+// console.log(s);
+
+// let b = 5;
+// let c = ++b;
+// console.log(b);
+// console.log(c);
